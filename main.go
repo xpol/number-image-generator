cda030a42efe88ce4c890f67e9935c97ae959c5c
Xpol Wan <xpolife@gmail.com>
2018-03-21 22:28:30 +0800

    Use user type to keep template resources.

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func check(err error) {
 	}
 }
 
-func toDigits(v int) ([]int) {
+func toDigits(v int) []int {
 	digits := make([]int, 0, 10)
 	for {
 		d := v % 10
@@ -28,7 +28,7 @@ func toDigits(v int) ([]int) {
 	return digits
 }
 
-func loadPng(filename string) (image.Image) {
+func loadPng(filename string) image.Image {
 	f, err := os.Open(filename)
 	check(err)
 	defer f.Close()
@@ -47,34 +47,40 @@ func savePng(filename string, img image.Image) {
 	check(png.Encode(f, img))
 }
 
-func loadImages() ([]image.Image, image.Image) {
-	images := make([]image.Image, 10)
-	for i := 0; i < 10; i++ {
-		images[i] = loadPng(fmt.Sprintf("template/%d.png", i))
-	}
-	background := loadPng("template/background.png")
-	return images, background
+type template struct {
+	digits     []image.Image
+	background image.Image
 }
 
+func loadImages() *template {
+	r := template{
+		digits:     make([]image.Image, 10),
+		background: loadPng("template/background.png"),
+	}
+	for i := 0; i < 10; i++ {
+		r.digits[i] = loadPng(fmt.Sprintf("template/%d.png", i))
+	}
+	return &r
+}
 
-func generateNumberImage(v int, images []image.Image, background image.Image) (image.Image) {
+func generateNumberImage(v int, t *template) image.Image {
 	digits := toDigits(v)
 
 	n := len(digits)
-	dr := images[8].Bounds()
-	ir := background.Bounds()
+	dr := t.digits[8].Bounds()
+	ir := t.background.Bounds()
 
 	p := image.Point{
-		X: (ir.Dx() - dr.Dx() * n) / 2,
+		X: (ir.Dx() - dr.Dx()*n) / 2,
 		Y: (ir.Dy() - dr.Dy()) / 2,
 	}
 
 	m := image.NewRGBA(ir.Bounds())
-	draw.Draw(m, m.Bounds(), background, image.ZP, draw.Src)
+	draw.Draw(m, m.Bounds(), t.background, image.ZP, draw.Src)
 
 	for i := len(digits) - 1; i >= 0; i-- {
 		dst := image.Rectangle{Min: p, Max: p.Add(dr.Size())}
-		draw.Draw(m, dst, images[digits[i]], image.ZP, draw.Over)
+		draw.Draw(m, dst, t.digits[digits[i]], image.ZP, draw.Over)
 		p.X += dr.Dx()
 	}
 
@@ -82,11 +88,11 @@ func generateNumberImage(v int, images []image.Image, background image.Image) (i
 }
 
 func main() {
-	images, background := loadImages()
+	t := loadImages()
 	_ = os.Mkdir("numbers", os.ModePerm)
 
 	for i := 1; i < 1000; i++ {
-		image := generateNumberImage(i, images, background)
+		image := generateNumberImage(i, t)
 		savePng(fmt.Sprintf("numbers/%03d.png", i), image)
 	}
 }
